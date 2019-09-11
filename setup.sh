fc052c13a940e81d6fc3c77dcdf21a0f87d09abb
alexrqs <lex.2418@gmail.com>
2019-09-11 17:13:11 -0500

    fix: add temp generator before copy the config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./.temp.generator.sh
+
 for file in .{aliases,bash_prompt,bashrc,dircolors,exports,functions,gitconfig,hyper.js,temp}; do
   [ -r "$file" ] && echo "ln $file ~/$file"
 done
