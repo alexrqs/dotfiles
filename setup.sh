5a520f595fff7df2bad4e885aa6da9ee6025b683
Alex <lex.2418@gmail.com>
2019-09-12 17:10:00 -0500

    fix: clean setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# ./.temp.generator.sh
-
 for file in .{aliases,bash_prompt,bashrc,dircolors,exports,functions,gitconfig,hyper.js,temp}; do
   [ -r "$file" ] && echo "ln $file ~/$file"
 done
