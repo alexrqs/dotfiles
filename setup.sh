4132cd543cfbaf86d1a9da60a988895ef50125ed
Alex <lex.2418@gmail.com>
2019-09-13 17:08:32 -0500

    feat: dynamic setup

--- setup.sh
+++ setup.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-./.temp.generator.sh
+# ./.temp.generator.sh
 
-ln $(pwd)/.aliases ~/.aliases
-echo "aliases was linked"
-
-ln $(pwd)/.dircolors ~/.dircolors
-echo "dircolors was linked"
-
-ln $(pwd)/.exports ~/.exports
-echo "exports was linked"
-
-ln $(pwd)/.functions ~/.functions
-echo "functions was linked"
-
-cp $(pwd)/.gitconfig ~/.gitconfig
-echo "gitconfig was copied"
-
-ln $(pwd)/.hyper.js ~/.hyper.js
-echo "hyper.js was linked"
-
-ln $(pwd)/.bash_prompt ~/.bash_prompt
-echo "bash_prompt was linked"
-
-ln $(pwd)/.bashrc ~/.bashrc
-echo "bashrc was linked"
-
-ln $(pwd)/.temp ~/.temp
-echo "temp was linked"
+for file in .{aliases,bash_prompt,bashrc,dircolors,exports,functions,gitconfig,hyper.js,temp}; do
+  [ -r "$file" ] && echo "ln $file ~/$file"
+done
