ebecb3b1d60174b3642823b498c74795bb659a7b
alexrqs <lex.2418@gmail.com>
2019-11-11 21:22:33 -0500

    mimic the real files

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 ./.temp.generator.sh
 
 for file in .{aliases,bash_prompt,bashrc,dircolors,exports,functions,gitconfig,hyper.js,temp}; do
-  [ -r "$file" ] && echo "ln $file ~/$file"
+  [ -r "$file" ] && echo "ln -s $(pwd)/$file ~/$file"
 done
