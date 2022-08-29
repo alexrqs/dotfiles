57f8f129f2d49793c504caa02341866a8457896e
alexrqs <lex.2418@gmail.com>
2022-08-29 09:42:32 -0500

    add zsh

--- .temp.generator.sh
+++ .temp.generator.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 echo "type your email"
-
 read email
+
+echo "type your user"
+read user
+
+echo "type your work email"
+read workEmail
+
+echo "type your work user"
+read workUser
 
 # clear .temp file
 > .temp
@@ -13,13 +21,18 @@ echo >> .temp
 
 echo "function me() {" >> .temp
 echo "  git config user.email \"${email}\"" >> .temp
+echo "  git config user.name \"${user}\"" >> .temp
 echo '  echo "local email $(git config user.email)"' >> .temp
+echo '  echo "local user $(git config user.name)"' >> .temp
 echo "}" >> .temp
 echo >> .temp
 
-echo "insert github token"
-read github_token
-
-echo "export GITHUB_TOKEN='${github_token}'" >> .temp
+echo "function work() {" >> .temp
+echo "  git config user.email \"${workEmail}\"" >> .temp
+echo "  git config user.name \"${workUser}\"" >> .temp
+echo '  echo "local email $(git config user.email)"' >> .temp
+echo '  echo "local user $(git config user.name)"' >> .temp
+echo "}" >> .temp
+echo >> .temp
 
 echo ".temp file created!"
