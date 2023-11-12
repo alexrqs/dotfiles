8a8ae256af9f9040f663923546cca4b0b8383d8c
alexrqs <lex.2418@gmail.com>
2023-11-11 21:06:28 -0500

    [add] lazy vim

---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        scope_incremental = false,
+      },
+    },
+  },
+}
