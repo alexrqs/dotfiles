c0566df7eed2344d2a7f13201ef95c00040b95c4
alexrqs <lex.2418@gmail.com>
2024-04-14 19:16:39 -0500

    test

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -2,6 +2,15 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = { "haydenmeade/neotest-jest" },
+    keys = {
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Run Last Test",
+      },
+    },
     opts = function(_, opts)
       table.insert(
         opts.adapters,
