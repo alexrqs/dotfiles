c03eb1f1a1b118e4b7c5ad2da7303143ca817366
alexrqs <lex.2418@gmail.com>
2024-05-22 16:32:52 -0500

    update http files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Register all http files to be actual http filetype
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.http",
+  command = "set filetype=http",
+})
