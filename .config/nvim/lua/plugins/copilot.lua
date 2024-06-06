b0927bb49b540c52fa3c0b89042aa46dcc051b97
alexrqs <lex.2418@gmail.com>
2024-06-06 16:23:58 -0500

    updates

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  "zbirenbaum/copilot-cmp",
+  config = function()
+    require("copilot_cmp").setup({
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+    })
+  end,
+}
