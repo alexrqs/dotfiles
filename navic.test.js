2b6bc9bd29e99092be275171baed1c3a75963ebf
Alex Lopez <alex@Alexs-MacBook-Pro.local>
2024-06-08 00:07:22 -0500

    conceal js

--- navic.test.js
+++ navic.test.js
@@ -1,9 +1,21 @@
-function hola() {
-  const foo = 'bar'
+function hola(param) {
+  const foo = 'bar' + param
 
   function chao() {
-    const bar = 'foo'
+    const bar = foo
+    const x = null
+
+    return bar + x
+  }
+
+  for (let i = 0; i < 10; i++) {
+    console.log(chao())
+  }
+
+  if (foo === 'bar' && param === 'hello') {
     return 'Chao'
   }
   return 'Hola'
 }
+
+hola('hello')
