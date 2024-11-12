03fb2bbccd3083b20bee30f7d73a3edf6423d7cd
alexrqs <lex.2418@gmail.com>
2024-11-12 10:49:52 -0500

    better terminal

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      integrations = {
+        cmp = true,
+        nvimtree = true,
+        telescope = true,
+        notify = true,
+        mini = true,
+      },
+    },
+  },
+}
